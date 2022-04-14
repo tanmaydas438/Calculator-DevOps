b31cb1912a3c4722c9a0bdac085130c421b1afa1
Tanmay <tanmaydas438@gmail.com>
2022-04-14 16:12:43 +0530

    added all the functions in the main class

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,104 +3,116 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class App {
-	private static final Logger logger = LogManager.getLogger(App.class);
+	private static final Logger loggen = LogManager.getLogger(App.class);
 	public static void main(String[] args){
-		Scanner reader = new Scanner(System.in);
-		int op, flag=0;
-		double num, exp;
-		int numm;
-		System.out.println("------------Calculator--------------");
+		Scanner sc = new Scanner(System.in);
+		int ch;
+		int number;
+		double number1;
+		double exp;
+		
+		System.out.println("!!!!!!!!!!!------------Calculator--------------!!!!!!!!!!!!");
 		do{
-			System.out.println("Choices of Operations:");
+			System.out.println("Enter you choice as per bellow options");
             System.out.println("");
-            System.out.println("1. Square root");
-            System.out.println("2. Factorial");
-            System.out.println("3. Natural Log");
-            System.out.println("4. Power");
-            System.out.println("5. Exit");
+            System.out.println("1. ----Square Root----");
+            System.out.println("2. ----Factorial----");
+            System.out.println("3. ----Natural Log(Base e)----");
+            System.out.println("4. ----Power----");
+            System.out.println("5. Exit !!!!!!!!!!!!");
 			System.out.println("");
             System.out.print("Enter your choice(number): ");
 			System.out.println("");
-			op = reader.nextInt();
-			if(op==5) flag = 1;
-			else{				
-				switch(op){
+			ch = sc.nextInt();
+			
+			switch(ch){
 					
-					case 1:  // Square Root
-						System.out.println("You choose Square Root!!");
+					case 1:  
+						System.out.println("----Square Root---");
 						System.out.println("");
-						System.out.print("Enter number: ");
-						num = reader.nextDouble();
-						squareRoot(num);
+						System.out.print("Please enter the number:");
+						number1 = sc.nextDouble();
+						getRoot(number1);
 					break;
 
-					case 2: //Factorial
-						System.out.println("You choose Factorial!!");
+					case 2: 
+						System.out.println("----Factorial----");
 						System.out.println("");
-						System.out.print("Enter number: ");
-						numm = reader.nextInt();
-						factorial(numm);
+						System.out.print("Please enter the number:");
+						number = sc.nextInt();
+						getFactorial(number);
 					break;
 
-					case 3: //Natural Log
-						System.out.println("You choose Natural Log!!");				
+					case 3: 
+						System.out.println("----Natural Log----");				
 						System.out.println("");
-						System.out.print("Enter number: ");
-						num = reader.nextDouble();
-						naturalLog(num);
+						System.out.print("Please enter the number:");
+						number1 = sc.nextDouble();
+						getNaturalLog(number1);
 					break;
 
-					case 4: //Power
-						System.out.println("You choose Power!!");
+					case 4: 
+						System.out.println("----Power----");
 						System.out.println("");
-						System.out.print("Enter number: ");
-						num = reader.nextDouble();
-						System.out.print("exponent: ");
-						exp = reader.nextDouble();
-						power(num,exp);
+						System.out.print("Please enter the number:");
+						number1 = sc.nextDouble();
+						System.out.print("Please enter the exponent:");
+						exp = sc.nextDouble();
+						getPower(number1,exp);
+					case 5:
+						exit(0);	
 					break;
 
 					default: 
-						System.out.println("Exiting due to invalid input!!");
-						flag = 1;
+						System.out.println("Please enter a valid choice");
+						
 					}
-				}		
-		    }while(flag == 0);
+						
+		    }while(1);
 		}
 
-	public static double squareRoot(double num){
-		double c = Math.sqrt(num);
+	public static void getRoot(double number){
+		double result = Math.sqrt(number);
 		System.out.println("");
-		System.out.println("The Result is "+c);
+		System.out.println("Square Root of "+number+" is:"+result);
 		System.out.println("");
-		logger.info("Executing squareRoot function!");
-		return c;
+		loggen.info("Executing squareRoot function........");
+		
 	}
 
-	public static int factorial(int num){
-	    int c = num ;
-		for(int i=num-1; i>=1; i--) c = c*i;
+	public static void getFactorial(int number){
+		if(number<0)
+			{
+			 System.out.println("oops!! you enter a negative number");
+			 return;
+			}
+		
+	    	int result = number ;
+		for(int i=num-1; i>=1; i--) 
+			result= result*i;
+		if(result==0)
+			result=1;
 		System.out.println("");
-		System.out.println("The Result is "+c);
+		System.out.println("Factorial of "+number+" is:"+result);
 		System.out.println("");
-		logger.info("Executing Factorial function!");
-		return c;
+		loggen.info("Executing Factorial function........");
+		
 	}
-	public static double naturalLog(double num){
-		double c = Math.log(num);	
+	public static void getNaturalLog(double number){
+		double result = Math.log(number);	
 		System.out.println("");
-		System.out.println("The Result is "+ c);
+		System.out.println("Log of "+number+" is:"+result);
 		System.out.println("");
-		logger.info("Executing naturalLog function!");
-		return c;
+		loggen.info("Executing naturalLog function........");
+		
 	}
-	  public static double power(double num, double exp){
-		double c = Math.pow(num,exp);
+	  public static void getPower(double number, double exponent){
+		double result = Math.pow(number,exponent);
 		System.out.println("");
-		System.out.println("The Result is "+ c);
+		System.out.println(number+" to the power "+exponent+" :"+result);
 		System.out.println("");
-		logger.info("Executing Power function!");
-		return c;
+		loggen.info("Executing Power function........");
+		
 	}
 
 }
